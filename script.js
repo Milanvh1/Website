c0af2a2e74338e4132c10a1a6b5e328f8fb240c8
Milan van Heijningen <21104875@student.hhs.nl>
2024-06-02 22:14:41 +0200

    personal website added

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
